d2acc430e14a04d73f4627db83d04e40cc7f41b1
Karan Pandit <70019018+babayaga110@users.noreply.github.com>
2024-11-21 14:43:01 +0530

    new updates

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { db_firestore } from "./firebase/config";
 import { collection, onSnapshot } from "firebase/firestore";
 import Marquee from "react-fast-marquee";
+import Template from "./Template";
 export default function App() {
   const [data, setData] = React.useState({});
   React.useEffect(() => {
@@ -20,41 +21,46 @@ export default function App() {
   }, []);
 
   return (
-    <div
-      className="flex justify-content-center align-items-center"
-      style={{ height: "calc(100vh - 50px)" }}
-    >
+    <div  style={{ height: "100vh" }} >
       <div
-        className={`relative basis-3/4  border-t-[10px] border-b-[10px] ${
-          data?.available ? "border-green-500" : "border-red-600"
-        }`}
+        className="flex justify-content-center align-items-center w-full h-[calc(100vh-50px)]"
       >
-        <div className="absolute top-2 right-2 bg-red-600 text-white w-fit h-fit font-bold z-10 ">
-          {data?.available ?  <p className='py-2 px-3 shadow-md bg-green-600'>
-              Doctor available
-            </p> :<p className='py-2 px-3 shadow-md bg-red-600'> 
-            Doctor on Duty</p>}
-          
-           
-          <span className={`absolute top-1 -right-1 w-full h-full -z-10 shadow-md ${data?.available ? "bg-green-300": "bg-red-300"}`}></span>
-        </div>
-        <img
+        <div
+          className={`relative basis-3/4  border-t-[10px] border-b-[10px] ${
+            data?.available ? "border-green-500" : "border-red-600"
+          }`}
+        >
+          <div className="absolute top-2 right-2 bg-red-600 text-white w-fit h-fit font-bold z-10 ">
+            {data?.available ? (
+              <p className="py-2 px-3 shadow-md bg-green-600">
+                Doctor available
+              </p>
+            ) : (
+              <p className="py-2 px-3 shadow-md bg-red-600">Doctor on Duty</p>
+            )}
+
+            <span
+              className={`absolute top-1 -right-1 w-full h-full -z-10 shadow-md ${
+                data?.available ? "bg-green-300" : "bg-red-300"
+              }`}
+            ></span>
+          </div>
+          {/* <img
           className="w-full h-full object-contain"
-          src={
-            data?.nfcToken ||
-            "https://firebasestorage.googleapis.com/v0/b/nfc-demo-d3f3b.firebasestorage.app/o/2022-11-27.jpg?alt=media&token=9e293a52-de74-4584-ba33-a4a34ccb189d"
-          }
+          src="https://firebasestorage.googleapis.com/v0/b/nfc-demo-d3f3b.firebasestorage.app/o/2022-11-27.jpg?alt=media&token=9e293a52-de74-4584-ba33-a4a34ccb189d"
           alt="placeholder"
-        />
+        /> */}
+          <Template doctor={data} />
+        </div>
+        <div className="basis-1/4">
+          <img
+            className="w-full h-full object-cover"
+            src={data?.gif || "http://via.placeholder.com/400x360"}
+            alt="placeholder"
+          />
+        </div>
       </div>
-      <div className="basis-1/4">
-        <img
-          className="w-full h-full object-cover"
-          src={data?.gif || "http://via.placeholder.com/400x360"}
-          alt="placeholder"
-        />
-      </div>
-      <div className="bg-[#F5EDED] w-full h-[50px] fixed bottom-0 flex items-center">
+      <div className="bg-[#F5EDED] w-full h-[50px]  flex items-center">
         <Marquee>
           <p className="text-2xl font-bold text-gray-800">
             Welcome to Hospital
---
+++ src/Template.jsx
@@ -0,0 +1,44 @@
+import * as React from "react";
+export default function Template({
+  doctor
+}) {
+  return (
+    <div className="w-full h-full flex flex-row">
+      <div className="w-full md:w-[35%] flex flex-col items-start justify-between p-2">
+        <img
+          src="https://cdn.prod.website-files.com/631e26f79f2c5af80112309a/6364dd74dc73e00bb8608349_6355bd38d13ee9a99e5b62eb_DSFH-Riyadh-Logo-p-500.webp"
+          alt="logo"
+          className="w-full h-auto object-contain "
+        />
+        <div className="w-full px-1  md:p-5">
+          <h4 className="font-bold text-gray-600 text-2xl md:text-3xl lg:text-3xl mb-2">
+            {doctor?.name?.en}
+          </h4>
+          <h4 className="font-bold text-gray-600 text-2xl md:text-3xl lg:text-3xl mb-2">
+            {doctor?.name?.ar}
+          </h4>
+          <hr className="w-full border-1 border-gray-300 mb-2 md:mb-5 " />
+          <p className="font-normal text-gray-400 text-xl md:text-2xl lg:text-2xl">
+            {doctor?.designation?.en}
+          </p>
+          <p className="font-normal text-gray-400 text-xl md:text-2xl lg:text-2xl">
+            {doctor?.designation?.ar}
+          </p>
+        </div>
+      </div>
+
+      <div className="w-[65%] bg-[#f99816] p-3 rounded-bl-[500px] flex justify-center items-center overflow-hidden">
+        <img
+          // src={doctor?.image}
+          src="https://avatars.githubusercontent.com/u/25135837?v=4"
+          alt="logo"
+          className="rounded-full object-cover"
+          style={{
+            aspectRatio: "1/1",
+            maxWidth:"80%"
+          }}
+        />
+      </div>
+    </div>
+  );
+}
